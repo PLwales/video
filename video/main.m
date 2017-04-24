ed76212ba38ec853a2fda46e36a673f6f75a2132
fj-zkyc <912796116@qq.com>
2017-04-24 11:48:28 +0800

    Initial Commit

---
+++ video/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  video
+//
+//  Created by fj-zkyc on 17/4/24.
+//  Copyright © 2017年 PLwales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
